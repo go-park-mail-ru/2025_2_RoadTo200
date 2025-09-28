351eea03fb3205cc5b437e4dbeb03f79fe1bbaa6
ender019 <gilkr125@gmail.com>
2025-09-28 20:44:55 +0300

    Допустимый адрес

--- backend/cors.go
+++ backend/cors.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 )
 
+const origin = "http://127.0.0.1:8001"
+
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Разрешаем запросы с любого origin (замените на конкретный домен для продакшена)
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		// Разрешаем методы
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		// Разрешаем заголовки
