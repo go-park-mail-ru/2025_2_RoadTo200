8767216419c410b5753e0b15f5a2e0abca046989
ender019 <gilkr125@gmail.com>
2025-10-27 14:45:58 +0300

    Подклбчение к базам

---
+++ backend/pkg/postgres/connect.go
@@ -0,0 +1,49 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"os"
+	"strconv"
+
+	"github.com/go-park-mail-ru/2025_2_RoadTo200/backend/internal/config"
+)
+
+func NewConnect(ctx context.Context, cfg *config.PostgresConfig) (*pgxpool.Pool, error) {
+	host := os.Getenv(cfg.Host)
+	sport := os.Getenv(cfg.Port)
+	user := os.Getenv(cfg.User)
+	password := os.Getenv(cfg.Password)
+	base := os.Getenv(cfg.Base)
+	port, err := strconv.Atoi(sport)
+	if err != nil {
+		return nil, err
+	}
+
+	poolConfig, err := pgxpool.ParseConfig(
+		fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user, password, host, port, base))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pool config: %w", err)
+	}
+
+	poolConfig.MaxConns = cfg.MaxConns
+	poolConfig.MinConns = cfg.MinConns
+	poolConfig.MaxConnLifetime = cfg.MaxLife
+	poolConfig.MaxConnIdleTime = cfg.MaxIdle
+	poolConfig.HealthCheckPeriod = cfg.HealthCheckInterval
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create connection pool: %w", err)
+	}
+
+	// Test connection
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return pool, nil
+}
