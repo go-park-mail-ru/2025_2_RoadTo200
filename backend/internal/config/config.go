aee6f9e0db60de4ba3df4a828c1785ba0e608439
ender019 <gilkr125@gmail.com>
2025-10-27 14:45:22 +0300

    Дополнение конфигурации

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
@@ -25,18 +26,27 @@ type LoggerConfig struct {
 }
 
 type PostgresConfig struct {
-	Host     string `yaml:"host"`
-	Port     string `yaml:"port"`
-	Base     string `yaml:"base"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
+	Host                string        `yaml:"host"`
+	Port                string        `yaml:"port"`
+	Base                string        `yaml:"base"`
+	User                string        `yaml:"user"`
+	Password            string        `yaml:"password"`
+	MinConns            int32         `yaml:"min_conns"`
+	MaxConns            int32         `yaml:"max_conns"`
+	MaxLife             time.Duration `yaml:"max_life"`
+	MaxIdle             time.Duration `yaml:"max_idle"`
+	HealthCheckInterval time.Duration `yaml:"health_check_interval"`
 }
 
 type RedisConfig struct {
-	Host     string `yaml:"host"`
-	Port     string `yaml:"port"`
-	Base     string `yaml:"base"`
-	Password string `yaml:"password"`
+	Host        string        `yaml:"host"`
+	Port        string        `yaml:"port"`
+	Base        string        `yaml:"base"`
+	Password    string        `yaml:"password"`
+	MaxConn     int           `yaml:"max_conn"`
+	MaxIdle     int           `yaml:"max_idle"`
+	MaxActive   int           `yaml:"max_active"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
 }
 
 func NewConfig() (*Config, error) {
@@ -48,6 +58,19 @@ func NewConfig() (*Config, error) {
 	}
 
 	var config appConfig
+	config.App.Postgres = PostgresConfig{
+		MinConns:            1,
+		MaxConns:            5,
+		MaxLife:             time.Hour,
+		MaxIdle:             30 * time.Minute,
+		HealthCheckInterval: time.Minute,
+	}
+	config.App.Redis = RedisConfig{
+		MaxConn:     5,
+		MaxIdle:     10,
+		MaxActive:   0,
+		IdleTimeout: 240 * time.Second,
+	}
 	if err := yaml.Unmarshal(data, &config); err != nil {
 		return nil, fmt.Errorf("failed to parse config: %w", err)
 	}
