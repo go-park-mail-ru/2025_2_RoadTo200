e3bef5104217c63eaa2b90487162fc084e468bbc
ender019 <gilkr125@gmail.com>
2025-10-29 14:32:26 +0300

    Скрипты для миграций.

---
+++ backend/migrations/drop.sql
@@ -0,0 +1,6 @@
+drop table user_preference cascade;
+drop table user_photo cascade;
+drop table subscription cascade;
+drop table match cascade;
+drop table swipe cascade;
+drop table "user" cascade;
