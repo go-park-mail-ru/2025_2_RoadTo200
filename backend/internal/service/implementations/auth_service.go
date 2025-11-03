b2ae4ce5fa5b2d3a74a912ae4d664bfcb07839e0
Дмитрий Егоров <egorov.dima1470@gmail.com>
2025-11-03 21:05:40 +0300

    fix migrations

--- backend/internal/service/implementations/auth_service.go
+++ backend/internal/service/implementations/auth_service.go
@@ -53,7 +53,8 @@ func (s *AuthService) Register(email, password, passwordConfirm string) (*domain
 		ID:         uuid.New(),
 		Email:      email,
 		Password:   string(hashedPassword),
-		Name:       "", // можно генерировать или оставить пустым
+		Name:       "",     // можно генерировать или оставить пустым
+		Gender:     "male", // ← пустая строка вместо NULL
 		IsVerified: false,
 		CreatedAt:  time.Now(),
 		UpdatedAt:  time.Now(),
