21f52f8540213e9140eb748cbcccef3bf979f8d4
ender019 <gilkr125@gmail.com>
2025-11-03 20:01:40 +0300

    Переработка репозиториев

--- backend/internal/repository/implementations/postgres/subscription_repository.go
+++ backend/internal/repository/implementations/postgres/subscription_repository.go
@@ -8,11 +8,14 @@ import (
 	domain "github.com/go-park-mail-ru/2025_2_RoadTo200/backend/internal/domain/entities"
 	"github.com/go-park-mail-ru/2025_2_RoadTo200/backend/internal/repository/interfaces"
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v4"
 )
 
-func NewSubscriptionRepository(pool *pgxpool.Pool) interfaces.SubscriptionRepository {
+type subscriptionRepository struct {
+	pool interfaces.PgxIface
+}
+
+func NewSubscriptionRepository(pool interfaces.PgxIface) interfaces.SubscriptionRepository {
 	return &subscriptionRepository{pool: pool}
 }
 
