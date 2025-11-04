1383d2b9c968e2c0baa219b98510c05b9103fcd4
ender019 <gilkr125@gmail.com>
2025-11-04 22:24:29 +0300

    Мидлвейр логирования

---
+++ backend/internal/handler/middleware/logging.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/go-park-mail-ru/2025_2_RoadTo200/backend/internal/logger"
+)
+
+func LogMiddleware(l *logger.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			l.Infof("%s %s", r.Method, r.RequestURI)
+			l.Debugf("Headers: %v", r.Header)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
