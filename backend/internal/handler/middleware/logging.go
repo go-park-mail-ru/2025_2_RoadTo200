841ac749d8cad5ea2b4219fdfc1ab28ddc2b25a5
ender019 <gilkr125@gmail.com>
2025-11-07 18:58:47 +0300

    Лог тела запроса

--- backend/internal/handler/middleware/logging.go
+++ backend/internal/handler/middleware/logging.go
@@ -11,6 +11,7 @@ func LogMiddleware(l *logger.Logger) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			l.Infof("%s %s", r.Method, r.RequestURI)
 			l.Debugf("Headers: %v", r.Header)
+			l.Debugf("Body: %v", r.Body)
 
 			next.ServeHTTP(w, r)
 		})
