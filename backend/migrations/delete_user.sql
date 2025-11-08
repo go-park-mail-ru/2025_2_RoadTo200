cfffbaba9d84b2137cade7466741f16f0228155f
Дмитрий Егоров <egorov.dima1470@gmail.com>
2025-11-08 06:14:36 +0300

    delete test users

--- backend/migrations/delete_user.sql
+++ backend/migrations/delete_user.sql
@@ -1,3 +1,12 @@
 DELETE FROM "user" WHERE id IN (
-    '65b1c564-1f77-4ee9-b23b-339803f694e1'
+    '38a85b39-ce18-4f8b-afa8-dc7890991af6',
+    '4ed9e505-ed39-442b-9ca5-80e37c9e1c48',
+    '0675a7e5-ccb8-4a06-899c-00dba717089a',
+    '9acf064f-1a2a-45eb-9c9f-9114ee4e805c',
+    'a99fbea3-8849-41d0-b56a-4c2236ed739a',
+    'f42e64cc-11ae-4276-af1a-72dce41b938d',
+    'f8f3aae5-533f-49e9-ba08-eabbbc72a893',
+    'd38b2180-d093-42b0-8200-8d9f75336775',
+    'a3370f8a-ecc3-4f83-9fb2-53618edf1718',
+    'a465d345-d5e0-4385-9644-a311cbfd48c4'
 );
