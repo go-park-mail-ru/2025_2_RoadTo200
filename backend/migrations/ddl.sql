84aeb1b20012f6a85014a5a7ac217a7fb853c27f
Дмитрий Егоров <egorov.dima1470@gmail.com>
2025-11-08 04:41:19 +0300

    back

--- backend/migrations/ddl.sql
+++ backend/migrations/ddl.sql
@@ -20,8 +20,6 @@ CREATE TABLE "user"
     birth_date  DATE,
     gender      gender_enum,
     bio         TEXT,
-    artist TEXT,
-    quote TEXT,
     latitude    DECIMAL(10, 8),
     longitude   DECIMAL(11, 8),
     is_verified BOOLEAN     NOT NULL DEFAULT FALSE,
