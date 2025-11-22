d28a0ce3ddc85031721e39df77f17261c09d5d73
ender019 <gilkr125@gmail.com>
2025-11-22 23:18:47 +0300

    Рабочие тесты для minio

--- internal/repository/interfaces/db_interfaces.go
+++ internal/repository/interfaces/db_interfaces.go
@@ -2,12 +2,22 @@ package interfaces
 
 import (
 	"context"
+	"io"
+	"net/url"
+	"time"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/minio/minio-go/v7"
 )
 
 type PgxIface interface {
 	pgx.Tx
 	Begin(context.Context) (pgx.Tx, error)
 	Close()
+}
+
+type MinioIface interface {
+	PutObject(context.Context, string, string, io.Reader, int64, minio.PutObjectOptions) (minio.UploadInfo, error)
+	RemoveObject(context.Context, string, string, minio.RemoveObjectOptions) error
+	PresignedGetObject(context.Context, string, string, time.Duration, url.Values) (*url.URL, error)
 }
