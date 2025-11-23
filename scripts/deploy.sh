10e4234e31c278760825b12ab59a991997300b8c
ender019 <gilkr125@gmail.com>
2025-11-24 00:41:53 +0300

    Фиксы swagger

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,19 +2,9 @@
 
 HOST=ubuntu.vk # Хост ОС. Пример: user@ххх.хх.хх.ххх
 DOCS=1     # Флаг сборки документации
-CONF=1     # Флаг отправки конфигурации
-MIGR=1
-BUILD=1
-
-if [[ $BUILD -eq 1 ]]; then
-  echo  "Building..."
-  make build-bin || exit 1
-
-  echo "Deploy binary file"
-  ssh ubuntu.vk sudo systemctl stop app-back.service
-  scp ./.build/* $HOST:/home/ubuntu/app/back/bin/ || echo "Error deploy binary"
-  ssh ubuntu.vk sudo systemctl start app-back.service
-fi
+CONF=0     # Флаг отправки конфигурации
+MIGR=0
+BILD=0
 
 if [[ $MIGR -eq 1 ]]; then
   echo "Deploy migration files"
@@ -35,4 +25,14 @@ fi
 if [[ $CONF -eq 1 ]]; then
   echo "Deploy config file"
   scp ./config/* $HOST:/home/ubuntu/app/back/config/ || echo "Error deploy config"
+fi
+
+if [[ $BILD -eq 1 ]]; then
+  echo  "Building..."
+  make build-bin || exit 1
+
+  echo "Deploy binary file"
+  ssh ubuntu.vk sudo systemctl stop auth-trb.service chat-trb.service core-trb.service app-back.service
+  scp ./.build/* $HOST:/home/ubuntu/app/back/bin/ || echo "Error deploy binary"
+  ssh ubuntu.vk sudo systemctl start auth-trb.service chat-trb.service core-trb.service app-back.service
 fi
