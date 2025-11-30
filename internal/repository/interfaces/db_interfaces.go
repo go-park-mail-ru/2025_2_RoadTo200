0ec707f1124c8d5af046134f6b6b21b372ba2524
ender019 <gilkr125@gmail.com>
2025-11-30 19:15:12 +0300

    Revert "Перемещение интерфейсов БД"
    This reverts commit c17860eda9423bcfcccf8d18c0f9c53d0261943c.

--- internal/repository/interfaces/db_interfaces.go
+++ internal/repository/interfaces/db_interfaces.go
@@ -1,10 +1,14 @@
-package postgres
+package interfaces
 
 import (
 	"context"
+	"io"
+	"net/url"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/minio/minio-go/v7"
 )
 
 type PgxIface interface {
@@ -14,4 +18,10 @@ type PgxIface interface {
 	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
 	Close()
+}
+
+type MinioIface interface {
+	PutObject(context.Context, string, string, io.Reader, int64, minio.PutObjectOptions) (minio.UploadInfo, error)
+	RemoveObject(context.Context, string, string, minio.RemoveObjectOptions) error
+	PresignedGetObject(context.Context, string, string, time.Duration, url.Values) (*url.URL, error)
 }
