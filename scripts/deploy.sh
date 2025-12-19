d0e585afbe18a8117b6d9b13a910b9e3d2fa7511
ender019 <gilkr125@gmail.com>
2025-12-19 16:41:25 +0300

    Easyjson

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-HOST=ubuntu.vk # Хост ОС. Пример: user@ххх.хх.хх.ххх
-DOCS=0     # Флаг сборки документации
+HOST=$1
+if [[ -z $HOST ]]; then
+  HOST=ubuntu.vk # Хост ОС. Пример: user@ххх.хх.хх.ххх
+fi
+DOCS=1     # Флаг сборки документации
 CONF=1     # Флаг отправки конфигурации
-MIGR=0
-INFR=0
+MIGR=1
+INFR=1
 BILD=1
 
 if [[ $MIGR -eq 1 ]]; then
   echo "Deploy migration files"
-  scp -r ./migrations/* $HOST:/home/ubuntu/app/back/data/migrations/ || echo "Error deploy migrations"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key -r ./migrations/* $HOST:/home/ubuntu/app/back/data/migrations/ || echo "Error deploy migrations"
 fi
 
 if [[ $DOCS -eq 1 ]]; then
   echo "Building swagger docs"
   make build-docs &&
   echo "Deploy swagger file"
-  scp ./api/auth/swagger.json $HOST:/home/ubuntu/app/back/data/docs/auth.json || echo "Error deploy auth swagger"
-  scp ./api/core/swagger.json $HOST:/home/ubuntu/app/back/data/docs/core.json || echo "Error deploy core swagger"
-  scp ./api/chat/swagger.json $HOST:/home/ubuntu/app/back/data/docs/chat.json || echo "Error deploy server swagger"
-  scp ./api/server/swagger.json $HOST:/home/ubuntu/app/back/data/docs/server.json || echo "Error deploy server swagger"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./api/auth/swagger.json $HOST:/home/ubuntu/app/back/data/docs/auth.json || echo "Error deploy auth swagger"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./api/core/swagger.json $HOST:/home/ubuntu/app/back/data/docs/core.json || echo "Error deploy core swagger"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./api/chat/swagger.json $HOST:/home/ubuntu/app/back/data/docs/chat.json || echo "Error deploy server swagger"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./api/server/swagger.json $HOST:/home/ubuntu/app/back/data/docs/server.json || echo "Error deploy server swagger"
 fi
 
 
 if [[ $CONF -eq 1 ]]; then
   echo "Deploy config file"
-  scp ./config/* $HOST:/home/ubuntu/app/back/config/ || echo "Error deploy config"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./config/* $HOST:/home/ubuntu/app/back/config/ || echo "Error deploy config"
 fi
 
 if [[ $INFR -eq 1 ]]; then
   echo "Deploy infra file"
-  scp ./docker-compose.yml $HOST:/home/ubuntu/app/configs/docker-compose.yaml || echo "Error deploy docker config"
-  scp -r ./infra/* $HOST:/home/ubuntu/app/configs/infra/ || echo "Error deploy metrics config"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./docker-compose.yml $HOST:/home/ubuntu/app/configs/docker-compose.yaml || echo "Error deploy docker config"
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key -r ./infra/* $HOST:/home/ubuntu/app/configs/infra/ || echo "Error deploy metrics config"
 fi
 
 if [[ $BILD -eq 1 ]]; then
@@ -39,8 +42,8 @@ if [[ $BILD -eq 1 ]]; then
   make build-bin || exit 1
 
   echo "Deploy binary file"
-  ssh ubuntu.vk sudo systemctl stop auth-trb.service chat-trb.service core-trb.service app-back.service
-  scp ./.build/* $HOST:/home/ubuntu/app/back/bin/ || echo "Error deploy binary"
-  ssh ubuntu.vk sudo systemctl start auth-trb.service chat-trb.service core-trb.service app-back.service
-  ssh ubuntu.vk /home/ubuntu/app/back/app.sh status
+  ssh -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key $HOST sudo systemctl stop auth-trb.service chat-trb.service core-trb.service app-back.service
+  scp -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key ./.build/* $HOST:/home/ubuntu/app/back/bin/ || echo "Error deploy binary"
+  ssh -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key $HOST sudo systemctl start auth-trb.service chat-trb.service core-trb.service app-back.service
+  ssh -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key $HOST /home/ubuntu/app/back/app.sh status
 fi
