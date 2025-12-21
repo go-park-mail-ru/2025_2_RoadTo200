c856ec888a90c3d1bf5be76995df2823f20a7c2e
ender019 <gilkr125@gmail.com>
2025-12-21 16:14:29 +0300

    Final test script

--- tests/perf_test/run_script.sh
+++ tests/perf_test/run_script.sh
@@ -7,26 +7,21 @@ API_URL=${2:-"https://terabithia.online"}
 WRITE_URL="register"
 READ_URL="login"
 RESULTS_DIR="results"
+RESPS=30
 mkdir -p "$RESULTS_DIR"
 
 echo "Цель: $API_URL"
 echo ""
 
 test_create() {
-    echo "📝 Тестирование создания 100k сущностей..."
+    wrk -t10 -c${RESPS} -d60s -s register.lua --latency "${API_URL}/api/${WRITE_URL}" > "${RESULTS_DIR}/create_report.txt"
 
-    wrk -t10 -c1000 -d100s -s register.lua --latency "${API_URL}/api/${WRITE_URL}" > "${RESULTS_DIR}/create_report.txt"
-
-    echo "✅ Создание завершено. RPS: 100, Длительность: 100 с"
     cat "${RESULTS_DIR}/create_report.txt"
 }
 
 test_read() {
-    echo "👁️ Тестирование чтения сущностей..."
+    wrk -t10 -c${RESPS} -d60s -s login.lua --latency "${API_URL}/api/${READ_URL}" > "${RESULTS_DIR}/read_report.txt"
 
-    wrk -t10 -c1000 -d100s -s login.lua --latency "${API_URL}/api/${READ_URL}" > "${RESULTS_DIR}/read_report.txt"
-
-    echo "✅ Чтение завершено. RPS: 100, Длительность: 5 с"
     cat "${RESULTS_DIR}/read_report.txt"
 }
 
